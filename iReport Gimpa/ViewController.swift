ad044b971b9774f753e2adc8841ea8e6299cf483
Kwesi Cobbina <kwesicobbina@Kwesis-Mac.local>
2017-04-30 07:08:33 +0000

    Initial Commit

---
+++ iReport Gimpa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iReport Gimpa
+//
+//  Created by Kwesi Cobbina on 30/04/2017.
+//  Copyright © 2017 Kwesi Cobbina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
